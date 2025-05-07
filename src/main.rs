db9437350e3f972beb6219a1f3b4070d329aa9c1
Adrian Papari <adrian.papari@assaabloy.com>
2025-05-07 15:33:10 +0200

    yolo

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,9 @@ fn run() -> Result<(), Box<Error>> {
     let document = web_sys::window()
         .ok_or(Error::UnableToRetrieveWindow)
         .and_then(|w| w.document().ok_or(Error::UnableToRetrieveDocument))?;
-    
-    let canvas = document.query_selector("canvas")
-        .map_err(|_| Error::UnableToRetrieveCanvas)?
-        .ok_or(Error::UnableToRetrieveCanvas)?
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| Error::UnableToRetrieveCanvas)?;
-    
-    let gl = canvas.get_context("webgl2")
-        .map_err(|_| Error::FailedToRetrieveWebGl2RenderingContext)?
-        .ok_or(Error::FailedToRetrieveWebGl2RenderingContext)?
-        .dyn_into::<web_sys::WebGl2RenderingContext>()
-        .map_err(|_| Error::FailedToRetrieveWebGl2RenderingContext)?;
+
+    let canvas = canvas_by_selector(&document, "canvas")?;
+    let gl = get_webgl2_rendering_context(&canvas)?;
     
     let shader = gl::ShaderProgram::create(&gl, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE)?;
     shader.use_program(&gl);
@@ -38,23 +29,51 @@ fn run() -> Result<(), Box<Error>> {
 
     // Draw a point
     gl.draw_arrays(web_sys::WebGl2RenderingContext::POINTS, 0, 1);
-    
+
     Ok(())
+}
+
+fn get_webgl2_rendering_context(
+    canvas: &web_sys::HtmlCanvasElement
+) -> Result<web_sys::WebGl2RenderingContext, Error> {
+    let gl = canvas.get_context("webgl2")
+        .map_err(|_| Error::FailedToRetrieveWebGl2RenderingContext)?
+        .ok_or(Error::FailedToRetrieveWebGl2RenderingContext)?
+        .dyn_into::<web_sys::WebGl2RenderingContext>()
+        .map_err(|_| Error::FailedToRetrieveWebGl2RenderingContext)?;
+    Ok(gl)
+}
+
+fn canvas_by_selector(
+    document: &web_sys::Document,
+    id: &str
+) -> Result<web_sys::HtmlCanvasElement, Error> {
+    let canvas = document.query_selector(id)
+        .map_err(|_| Error::UnableToRetrieveCanvas)?
+        .ok_or(Error::UnableToRetrieveCanvas)?
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| Error::UnableToRetrieveCanvas)?;
+
+    Ok(canvas)
 }
 
 const VERTEX_SHADER_SOURCE: &str = indoc! { r#"
     #version 300 es
+
+    layout(location = 0) in vec2 a_pos;
+
     void main() {
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+        gl_Position = vec4(a_pos, 0.0, 1.0);
         gl_PointSize = 100.0;
     }
 "#};
 
 const FRAGMENT_SHADER_SOURCE: &str = indoc! {r#"
     #version 300 es
+
     precision mediump float;
     out vec4 FragColor;
-    
+
     void main() {
         FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
     }
