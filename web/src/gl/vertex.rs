7c5d78376e9e36f60fea688837d8530f6614d2dd
Adrian Papari <adrian.papari@assaabloy.com>
2025-05-17 20:06:14 +0200

    use VAO

--- web/src/gl/vertex.rs
+++ web/src/gl/vertex.rs
@@ -5,6 +5,7 @@ use bon::bon;
 use web_sys::{console, WebGl2RenderingContext};
 
 pub struct CellArray {
+    vao: web_sys::WebGlVertexArrayObject,
     vbo: web_sys::WebGlBuffer,
     instance_buf: web_sys::WebGlBuffer,
     index_buf: web_sys::WebGlBuffer,
@@ -32,21 +33,15 @@ impl CellArray {
         indices: &[u8],
         shader: &ShaderProgram,
     ) -> Result<Self, Error> {
+        // create VAO
+        let vao = gl.create_vertex_array()
+            .ok_or(Error::VertexArrayCreationError)?;
+        gl.bind_vertex_array(Some(&vao));
 
         // create and bind VBO
         let vbo = gl.create_buffer()
             .ok_or(Error::BufferCreationError("vbo"))?;
         gl.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&vbo));
-
-        let index_buf = gl.create_buffer()
-            .ok_or(Error::BufferCreationError("index buffer"))?;
-        gl.bind_buffer(WebGl2RenderingContext::ELEMENT_ARRAY_BUFFER, Some(&index_buf));
-
-        // upload vertex index data to GPU
-        unsafe {
-            let view = js_sys::Uint8Array::view(indices);
-            gl.buffer_data_with_array_buffer_view(GL::ELEMENT_ARRAY_BUFFER, &view, GL::STATIC_DRAW);
-        }
 
         // upload vertex data to GPU
         unsafe {
@@ -54,20 +49,57 @@ impl CellArray {
             gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &view, GL::STATIC_DRAW);
         }
 
-        // create and bind instance buffer
-        let instance_buf = create_instance_buffer(gl, transform_data)?;
+        // setup vertex attributes (while VAO is bound)
+        const STRIDE: i32 = (2 + 2) * 4; // 2+2 f32 for position and UV
+        gl.vertex_attrib_pointer_with_i32(Self::POS_ATTRIB, 2, GL::FLOAT, false, STRIDE, 0);
+        gl.enable_vertex_attrib_array(Self::POS_ATTRIB);
+        gl.vertex_attrib_pointer_with_i32(Self::UV_ATTRIB, 2, GL::FLOAT, false, STRIDE, 2 * 4);
+        gl.enable_vertex_attrib_array(Self::UV_ATTRIB);
 
-        // get Sampler uniform location
+        // create and bind instance buffer
+        let instance_buf = gl.create_buffer()
+            .ok_or(Error::BufferCreationError("instance buffer"))?;
+        gl.bind_buffer(GL::ARRAY_BUFFER, Some(&instance_buf));
+
+        // upload instance data
+        unsafe {
+            let data_ptr = transform_data.as_ptr() as *const u8;
+            let size = transform_data.len() * size_of::<InstanceData>();
+            let view = js_sys::Uint8Array::view(slice::from_raw_parts(data_ptr, size));
+            gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &view, GL::STATIC_DRAW);
+        }
+
+        // setup instance attributes (while VAO is bound)
+        use InstanceData as ID;
+        enable_vertex_attrib_array(gl, ID::POS_ATTRIB,   2, GL::UNSIGNED_SHORT,  0);
+        enable_vertex_attrib_array(gl, ID::DEPTH_ATTRIB, 1, GL::FLOAT,           4);
+        enable_vertex_attrib_array(gl, ID::FG_ATTRIB,    1, GL::UNSIGNED_INT,    8);
+        enable_vertex_attrib_array(gl, ID::BG_ATTRIB,    1, GL::UNSIGNED_INT,    12);
+
+        // create and bind index buffer (still part of VAO state)
+        let index_buf = gl.create_buffer()
+            .ok_or(Error::BufferCreationError("index buffer"))?;
+        gl.bind_buffer(WebGl2RenderingContext::ELEMENT_ARRAY_BUFFER, Some(&index_buf));
+
+        // upload index data
+        unsafe {
+            let view = js_sys::Uint8Array::view(indices);
+            gl.buffer_data_with_array_buffer_view(GL::ELEMENT_ARRAY_BUFFER, &view, GL::STATIC_DRAW);
+        }
+
+        // unbind VAO to prevent accidental modification
+        gl.bind_vertex_array(None);
+
+        // Get uniform locations
         let sampler_loc = gl.get_uniform_location(&shader.program, "u_sampler")
             .ok_or(Error::UnableToRetrieveUniformLocation("u_sampler"))?;
-
-        // get projection uniform location
         let projection_loc = gl.get_uniform_location(&shader.program, "u_projection")
             .ok_or(Error::UnableToRetrieveUniformLocation("u_projection"))?;
-        
+
         console::log_2(&"terminal cells".into(), &transform_data.len().into());
-        
+
         Ok(Self {
+            vao,
             vbo,
             index_buf,
             instance_buf,
@@ -77,31 +109,6 @@ impl CellArray {
             count: transform_data.len() as i32,
         })
     }
-}
-
-pub(crate) fn create_instance_buffer(
-    gl: &WebGl2RenderingContext,
-    instances: &[InstanceData],
-) -> Result<web_sys::WebGlBuffer, Error> {
-    let buffer = gl.create_buffer()
-        .ok_or(Error::BufferCreationError("instance buffer"))?;
-    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&buffer));
-
-    unsafe {
-        let data_ptr = instances.as_ptr() as *const u8;
-        let size = instances.len() * size_of::<InstanceData>();
-        let view = js_sys::Uint8Array::view(slice::from_raw_parts(data_ptr, size));
-        gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &view, GL::STATIC_DRAW);
-    }
-
-    use InstanceData as ID;
-    //                               attribute,    size,     type,         offset
-    enable_vertex_attrib_array(gl, ID::POS_ATTRIB,   2, GL::UNSIGNED_SHORT,  0);
-    enable_vertex_attrib_array(gl, ID::DEPTH_ATTRIB, 1, GL::FLOAT,           4);
-    enable_vertex_attrib_array(gl, ID::FG_ATTRIB,    1, GL::UNSIGNED_INT,    8);
-    enable_vertex_attrib_array(gl, ID::BG_ATTRIB,    1, GL::UNSIGNED_INT,    12);
-
-    Ok(buffer)
 }
 
 fn enable_vertex_attrib_array(
@@ -125,22 +132,10 @@ fn enable_vertex_attrib_array(
 
 impl Drawable for CellArray {
     fn bind(&self, gl: &WebGl2RenderingContext) {
-        gl.bind_buffer(GL::ARRAY_BUFFER, Some(&self.vbo));
-        gl.bind_buffer(GL::ELEMENT_ARRAY_BUFFER, Some(&self.index_buf));
+        gl.bind_vertex_array(Some(&self.vao));
 
-        // bind texture and set the sampler uniform to use texture unit 0
         self.atlas.bind(gl, 0);
         gl.uniform1i(Some(&self.sampler_loc), 0);
-
-        const STRIDE: i32 = (2 + 2) * 4; // 2+2 f32 for position and UV
-
-        // set up vertex attribute pointer
-        gl.vertex_attrib_pointer_with_i32(Self::POS_ATTRIB, 2, GL::FLOAT, false, STRIDE, 0);
-        gl.enable_vertex_attrib_array(Self::POS_ATTRIB);
-
-        // setup UV attribute pointer
-        gl.vertex_attrib_pointer_with_i32(Self::UV_ATTRIB, 2, GL::FLOAT, false, STRIDE, 2 * 4);
-        gl.enable_vertex_attrib_array(Self::UV_ATTRIB);
     }
 
     fn draw(&self, gl: &WebGl2RenderingContext) {
@@ -148,10 +143,7 @@ impl Drawable for CellArray {
     }
 
     fn unbind(&self, gl: &WebGl2RenderingContext) {
-        // unbind buffers to prevent accidental modification
-        gl.bind_buffer(GL::ELEMENT_ARRAY_BUFFER, None);
-        gl.bind_buffer(GL::ARRAY_BUFFER, None);
-        
+        gl.bind_vertex_array(None);
         gl.bind_texture(GL::TEXTURE_2D, None);
     }
 }
